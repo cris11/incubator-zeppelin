13027248ce368e6b7dbd3d9f6887f24d9c7d4f5c
root <root@ktmedia011.(none)>
2016-03-07 20:32:34 +0900

    enh7

--- zeppelin-ci/build/backends/spark_standalone/stop.sh
+++ zeppelin-ci/build/backends/spark_standalone/stop.sh
@@ -7,22 +7,14 @@ envfile=$3
 SPARK_SHARE=/reposhare/$BUILD_TYPE
 
 # ----------------------------------------------------------------------
-# Stopping Backend
+# Stopping spark
 # ----------------------------------------------------------------------
 SPARK_DAT=spark-$SPARK_VER-bin-hadoop$HADOOP_VER
 export SPARK_HOME="$SPARK_SHARE/$SPARK_DAT"
 cd $SPARK_HOME/sbin
 
-# stopping
 ./spark-daemon.sh stop org.apache.spark.deploy.worker.Worker 1
 ./stop-master.sh
-
-: ${HADOOP_PREFIX:=/usr/local/hadoop}
-##$HADOOP_PREFIX/etc/hadoop/hadoop-env.sh
-
-# stopping hadoop
-$HADOOP_PREFIX/sbin/stop-dfs.sh
-$HADOOP_PREFIX/sbin/stop-yarn.sh
 
 # ----------------------------------------------------------------------
 # End of Script
