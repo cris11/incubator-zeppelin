84d28a02111c8a1daae8aee87d3cdb84d709fd39
root <root@ktmedia011.(none)>
2016-03-07 17:24:14 +0900

    enh5

--- zeppelin-ci/build/script.sh
+++ zeppelin-ci/build/script.sh
@@ -7,6 +7,13 @@ envfile=$3
 SPARK_SHARE=/reposhare/$BUILD_TYPE
 SPARK_DAT=spark-${SPARK_VER}-bin-hadoop${HADOOP_VER}
 
+# confirm spark binary
+if [ ! -d $SPARK_SHARE/$SPARK_DAT ]; then
+	SPARK_BIN=$SPARK_DAT.tgz
+	tar xfz /reposhare/$SPARK_BIN -C $SPARK_SHARE
+fi
+
+# set spark home
 \cp -f /tmp/zeppelin-env.sh $zephome/conf/
 echo "export SPARK_HOME=$SPARK_SHARE/$SPARK_DAT" >> $zephome/conf/zeppelin-env.sh
 
@@ -15,5 +22,6 @@ if [[ $BUILD_TYPE == "spark_yarn" ]]; then
 	\cp -f /tmp/spark_conf/*  ${SPARK_SHARE}/${SPARK_DAT}/conf/
 fi
 
-cd $zephome
+# run scripts
+echo ""; cd $zephome
 $envhome/script.sh
