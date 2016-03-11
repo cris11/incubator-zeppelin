fddec42786ca455429640b0c326087970c8c287b
root <root@ktmedia011.(none)>
2016-03-11 14:57:05 +0900

    t

--- zeppelin-ci/build/script.sh
+++ zeppelin-ci/build/script.sh
@@ -7,18 +7,20 @@ envfile=$3
 #envitem=$4
 src="/zeppelin-${SPARK_VER}"
 #target="./zeppelin-${SPARK_VER}-test"
-target="./zeppelin-${SPARK_VER}-test"
+target="/zeppelin-${SPARK_VER}-test"
 SPARK_SHARE=/reposhare/$BUILD_TYPE
 SPARK_DAT=spark-${SPARK_VER}-bin-hadoop${HADOOP_VER}
 
-Xvfb $DISPLAY -ac -screen 0 1280x1024x24 > /reposhare/xvfb-${BUILD_TYPE}-${SPARK_VER}.log 2>&1 &
-pid=`echo $!`
-sleep 1
 
-echo "# Xvfb Starting..."
-echo "# PID : ${pid}"
-cat reposhare/xvfb-${BUILD_TYPE}-${SPARK_VER}.log
-echo ""
+#Xvfb $DISPLAY -ac -screen 0 1280x1024x24 > /reposhare/xvfb-${BUILD_TYPE}-${SPARK_VER}.log 2>&1 &
+#pid=`echo $!`
+#sleep 2
+
+#echo "# Xvfb Starting..."
+#echo "# PID : ${pid}"
+#cat reposhare/xvfb-${BUILD_TYPE}-${SPARK_VER}.log
+#echo ""
+
 
 # --------------------------------------------------
 # confirm spark binary
@@ -29,47 +31,47 @@ if [ ! -d $SPARK_SHARE/$SPARK_DAT ]; then
 fi
 
 # --------------------------------------------------
-# set spark home
+# copy installed source ( container aufs to host fs )
 # --------------------------------------------------
-\cp -f /tmp/zeppelin-env.sh $zephome/conf/
-echo "export SPARK_HOME=$SPARK_SHARE/$SPARK_DAT" >> $zephome/conf/zeppelin-env.sh
+if [ ! -d $target ]; then
+### ori ver
+#	cd $zephome; cd ..
+#	\cp -rf $src $target
 
-if [[ $BUILD_TYPE == "spark_yarn" ]]; then
-	echo "- copy spark conf."
-	\cp -f /tmp/spark_conf/*  ${SPARK_SHARE}/${SPARK_DAT}/conf/
+### test ver
+	\cp -rf /reposhare/zepp/$src $target
 fi
 
 # --------------------------------------------------
-# copy installed source ( container aufs to host fs )
+# set spark home
 # --------------------------------------------------
-cd $zephome; cd ..
-#if [ ! -d $target ]; then
-#	\cp -rf $src $target
-#fi
+#\cp -f /tmp/zeppelin-env.sh $zephome/conf/
+#echo "export SPARK_HOME=$SPARK_SHARE/$SPARK_DAT" >> $zephome/conf/zeppelin-env.sh
 
-### test ver
-\cp -rf /reposhare/zepp/$src $target
+if [[ $BUILD_TYPE == "spark_yarn" ]]; then
+	\cp -f /tmp/spark_conf/*  ${SPARK_SHARE}/${SPARK_DAT}/conf/
+fi
+\cp -f /tmp/zeppelin-env.sh $target/conf/
+echo "export SPARK_HOME=$SPARK_SHARE/$SPARK_DAT" >> $target/conf/zeppelin-env.sh
+
 
 # --------------------------------------------------
 # run scripts
 # --------------------------------------------------
-echo ""
 echo -n "# Current DIR : "
-pwd
-echo ""
+pwd; echo ""
 
 cd $target
 $envhome/script.sh
-ret=`echo $?`
 
 # --------------------------------------------------
 # remove source
 # --------------------------------------------------
 # Stop Xvfb
-echo "# Stopping Xvfb - PID(${pid})"
-if kill -0 $pid; then
-	kill $pid
-fi
+#echo "# Stopping Xvfb - PID(${pid})"
+#if kill -0 $pid; then
+#	kill $pid
+#fi
 
 echo "# remove souce"
 
